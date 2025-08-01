86fb470b4a342d507a066d3a6b7dab3e80879be9
Seidina Oumar <oumar4191@icloud.com>
2025-08-01 22:10:38 +0000

    Ajout de gunicorn avec succès

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Gestcom_coccinelle\.venv
+set VIRTUAL_ENV=C:\Users\User\Desktop\PFE\Gestcom_coccinelle\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
